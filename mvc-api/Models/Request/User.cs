5e56ceb83fd1eab840fd6b150b50acdd686980cb
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-01-22 16:07:23 +0900

    add 複数検証

--- mvc-api/Models/Request/User.cs
+++ mvc-api/Models/Request/User.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using System.Xml.Linq;
 
 namespace mvc_api.Models.Request
 {
+    [CustomValidation(typeof(User), "CheckNameAndPassBakaWord")]
     public class User
     {
         [Required(ErrorMessage = "{0} is required")]
         [StringLength(20, ErrorMessage = "{0} length cant'be more than {1}.")]
         [JsonPropertyName("login_name")]
         [Display(Name = "login_name")]
+        [CustomValidation(typeof(User), "CheckNgWord")]
         public string? LoginName { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
@@ -16,5 +19,33 @@ namespace mvc_api.Models.Request
         [JsonPropertyName("password")]
         [Display(Name = "password")]
         public string? Password { get; set; }
+
+        
+       /// <summary>
+       /// 独自検証
+       /// </summary>
+        public static ValidationResult CheckNgWord(string? name)
+        {
+            if (name?.ToLower().Contains("ngword") ?? false)
+            {
+                return new ValidationResult("NGワードあり");
+            }
+
+            return ValidationResult.Success!;
+        }
+
+        /// <summary>
+        /// 複数プロパティ検証
+        /// </summary>
+        public static ValidationResult CheckNameAndPassBakaWord (User? user)
+        {
+            if ((user?.LoginName?.ToLower().Contains("bakaword") ?? false)
+                    && (user.Password?.ToLower().Contains("bakaword") ?? false))
+            {
+                return new ValidationResult("NGワードあり",new List<string> { "loginName, Password"});
+            }
+
+            return ValidationResult.Success!;
+        }
     }
 }

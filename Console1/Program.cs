08a0d0b65c8a9769488f2bf9ffcee6c2fb698dfd
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-02-03 23:21:50 +0900

    コンソールにNlog対応

--- Console1/Program.cs
+++ Console1/Program.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.Extensions.Configuration;
+using NLog;
 
 namespace Console1
 {
     internal class Program
     {
+        static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            // 設定ファイル
             IConfiguration configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile(path: "appsettings.json")
                 .Build();
 
             Console.WriteLine(configuration["Url:WebAPIBaseUrl"]);
+
+            // ログ
+            for (int i = 0; i < 10; i++)
+            {
+                logger.Trace("tarace");
+                logger.Info("information");
+                logger.Warn("waraninig");
+                logger.Error("error");
+                logger.Fatal("fatal"); ;
+            }
+
+
+
+            Console.WriteLine("\npress enter key to exit...");
             Console.ReadLine();
-            
         }
     }
 }
